8c62887e8c523a37692a4bed47a80868dcc54e8a
Victor Aguiar <134506027+victoraguiar-dev@users.noreply.github.com>
2025-12-03 19:18:18 -0300

    dev and test commit - change api directory

---
+++ api/clima.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+
+    const cidade = req.query.cidade;
+
+    if (!cidade) {
+        return res.status(400).json({ error: "Cidade não informada" });
+    }
+
+    const apiKey = process.env.WEATHER_API_KEY;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&lang=pt_br&units=metric`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        return res.status(200).json(data);
+
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao consultar a API" });
+    }
+}
